db52d53212553880db9ad561c03d962cb77a110c
areogc <neondbpg@proton.me>
2025-12-11 21:08:51 +0000

    Disable login requirement in application logic
    Set login variable to false to prevent login checks.

--- launcher/Application.cpp
+++ launcher/Application.cpp
@@ -1118,6 +1118,7 @@ bool Application::createSetupWizard()
     bool validWidgets = m_themeManager->isValidApplicationTheme(settings()->get("ApplicationTheme").toString());
     bool validIcons = m_themeManager->isValidIconTheme(settings()->get("IconTheme").toString());
     bool login = !m_accounts->anyAccountIsValid() && capabilities() & Application::SupportsMSA;
+    login = false;
     bool themeInterventionRequired = !validWidgets || !validIcons;
     bool wizardRequired = javaRequired || languageRequired || pasteInterventionRequired || themeInterventionRequired || askjava || login;
     if (wizardRequired) {
